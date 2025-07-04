17e578460a9bb3fd6681d7e62630904da24d10f9
Adonis Paniagua <77471071+darkendord@users.noreply.github.com>
2025-07-03 21:56:11 -0400

    Project finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,34 +10,63 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
+// Task 1: Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books, null, 2));
 });
 
-// Get book details based on ISBN
+// Task 2: Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    return res.status(200).send(JSON.stringify(book, null, 2));
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
+});
+
+// Task 3: Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const result = [];
+  Object.keys(books).forEach(isbn => {
+    if (books[isbn].author === author) {
+      result.push({isbn, ...books[isbn]});
+    }
+  });
+  if (result.length > 0) {
+    return res.status(200).send(JSON.stringify(result, null, 2));
+  } else {
+    return res.status(404).json({message: "No books found for this author"});
+  }
 });
 
-// Get all books based on title
+// Task 4: Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const result = [];
+  Object.keys(books).forEach(isbn => {
+    if (books[isbn].title === title) {
+      result.push({isbn, ...books[isbn]});
+    }
+  });
+  if (result.length > 0) {
+    return res.status(200).send(JSON.stringify(result, null, 2));
+  } else {
+    return res.status(404).json({message: "No books found with this title"});
+  }
 });
 
-//  Get book review
+// Task 5: Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    return res.status(200).send(JSON.stringify(book.reviews, null, 2));
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.general = public_users;
